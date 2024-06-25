ae5d828c1a533e7ae085e963e18d7697e734d60a
Sam Nick <omprakash121samuni@gmail.com>
2024-06-26 00:10:48 +0530

    Added login route without logic

--- server/src/routes/auth/auth.js
+++ server/src/routes/auth/auth.js
@@ -3,7 +3,7 @@ const {Success} = require("../../helper/handleResponse");
 
 const router = express.Router()
 
-router.get('/login', async (req, res) => {
+router.post('/login', async (req, res) => {
     const data = {
         date: new Date().toLocaleString('en-US', {timeZone: 'Asia/Kolkata', hour12: true}),
         version: process.env.APP_VERSION
@@ -11,9 +11,9 @@ router.get('/login', async (req, res) => {
     return Success(res, {...data})
 })
 
-// router.post('/register', async (req, res) => {
-//     const date = new Date()
-//     res.json({date: date.toLocaleString('en-US', {timeZone: 'Asia/Kolkata', hour12: true})});
-// })
+router.post('/register', async (req, res) => {
+    const date = new Date()
+    res.json({date: date.toLocaleString('en-US', {timeZone: 'Asia/Kolkata', hour12: true})});
+})
 
 module.exports = router;
