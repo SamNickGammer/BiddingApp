c6f555575ab3173291018111d1d46c162f2f611d
Sam Nick <omprakash121samuni@gmail.com>
2024-07-02 15:01:36 +0530

    Added ESLITN

--- web/src/App.js
+++ web/src/App.js
@@ -1,10 +1,9 @@
+import React from "react"
 import './App.css';
 import {BrowserRouter} from "react-router-dom";
 import Routes from "./routes/RoutesNav";
 
 function App() {
-    // const isLearning = false;
-
     return (
         <div className="mx-auto max-w-screen px-4 sm:px-6 lg:px-8 ">
             <BrowserRouter>
--- web/src/utils/router.js
+++ web/src/utils/router.js
@@ -1,7 +1,7 @@
 export const routerNavigation = (navigation) => {
+    // eslint-disable-next-line no-undef
     const nodeEnv = process.env.NODE_ENV
     const hostName = "BiddingApp"
-    // console.log('RouterNavigation', navigation, process.env.NODE_ENV);
     if (nodeEnv !== 'development') {
         return `/${hostName}${navigation}`
     }
